9bbf86d3a4e4b6e476b5d7b4d0b898dcc498c119
sunneepark <hl2ozs@naver.com>
2020-06-06 00:45:25 +0900

    20/06/06 complete: command pattern 적용

--- LAB11/commandPattern/Client.java
+++ LAB11/commandPattern/Client.java
@@ -4,7 +4,7 @@ public class Client {
 	public static void main(String[] args) {
 		StockTrade stock = new StockTrade();
 		BuyStockOrder buyStockOrder = new BuyStockOrder(stock);
-		SellStockOrder sellStockOrder = new SellStockOrder();
+		SellStockOrder sellStockOrder = new SellStockOrder(stock);
 		Agent agent = new Agent();
 
 		agent.placeOrder(buyStockOrder); // Buy Shares
